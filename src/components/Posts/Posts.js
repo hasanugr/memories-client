dbe7aee8d5af5af5e13be47b436dbfb89b07c5f0
Hasan Uğur <hasan.ugur4535@gmail.com>
2021-10-25 19:07:04 +0300

    Listed posts ordered reverse

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -18,7 +18,7 @@ const Posts = ({ setCurrentId }) => {
       alignItems="stretch"
       spacing={3}
     >
-      {posts.map((post) => (
+      {[...posts].reverse().map((post) => (
         <Grid key={post._id} item xs={12} sm={6}>
           <Post post={post} setCurrentId={setCurrentId} />
         </Grid>
